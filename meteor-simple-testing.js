4defe2f06c8ec24fc3cea0fcd1958ce918e925f0
Tomas Romero <canotto90@gmail.com>
2015-06-19 23:41:30 -0300

    Initial commit

---
+++ meteor-simple-testing.js
@@ -0,0 +1,47 @@
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+
+    should()
+
+    describe('model example', function() {
+
+      Exams = new Mongo.Collection('exams')
+      Exams.insert({ score: 9 })
+
+      it('should have exams', function() {
+        Exams.find().count().should.be.above(1)
+      })
+
+    })
+
+    describe('simple math example', function() {
+
+      context('1 + 1', function() {
+
+        number = 1 + 1
+
+        it('should be 3 (expected failure)', function() {
+          number.should.equal(3)
+        })
+
+        it('should be greater than 1', function() {
+          number.should.be.above(1)
+        })
+
+        context('multiplied by 2', function() {
+
+          number = number * 2
+
+          it('should be 4', function() {
+            number.should.equal(4)
+          })
+        })
+
+      })
+
+    })
+
+    T.run()
+
+  })
+}
