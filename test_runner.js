898b3344b823b4d67e31d9c23dd55d6c144a1090
Tomas Romero <canotto90@gmail.com>
2015-06-20 23:02:47 -0300

    Extracted examples to sample project

---
+++ test_runner.js
@@ -0,0 +1,12 @@
+Meteor.startup(function() {
+
+  T.run(function() {
+    if (suiteIncludedCounter != 2) {
+      throw 'ignored `suite` tests that should not have been ignored'
+    }
+    if (itIncludedCounter != 1) {
+      throw 'ignored `it` tests that should not have been ignored'
+    }
+  })
+
+})
