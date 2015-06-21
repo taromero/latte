ff840819e5924df0f2cf7f8fe556a756947daad5
Tomas Romero <canotto90@gmail.com>
2015-06-20 21:33:14 -0300

    Added some jasmine specs that apply to latte

---
+++ specs/jasmine_specs.js
@@ -0,0 +1,130 @@
+should()
+
+// Limited set of jasmine generic specs (from http://jasmine.github.io/2.3/introduction.html)
+T.prepare(function() {
+
+  // Introduction
+
+  describe("A suite", function() {
+
+    it("contains spec with an expectation", function() {
+      true.should.be.true
+    })
+
+  })
+
+  describe("A suite is just a function", function() {
+    var a;
+
+    it("and so is a spec", function() {
+      a = true;
+
+      a.should.be.true
+    })
+  })
+
+  // Grouping Related Specs with describe
+
+  describe("A spec", function() {
+    it("is just a function, so it can contain any code", function() {
+      var foo = 0
+      foo += 1
+
+      expect(foo).to.eq(1)
+    })
+
+    it("can have more than one expectation", function() {
+      var foo = 0
+      foo += 1
+
+      expect(foo).to.eq(1)
+      expect(true).to.eq(true)
+    })
+
+  })
+
+  // Setup and Teardown
+
+  describe("A spec using beforeEach and afterEach", function() {
+    var foo = 0
+
+    beforeEach(function() {
+      foo += 1
+    })
+
+    afterEach(function() {
+      foo = 0
+    })
+
+    it("is just a function, so it can contain any code", function() {
+      expect(foo).to.eq(1)
+    })
+
+    it("can have more than one expectation", function() {
+      expect(foo).to.eq(1)
+      expect(true).to.eq(true)
+    })
+
+  })
+
+  describe("A spec using beforeAll and afterAll", function() {
+    var foo
+
+    beforeAll(function() {
+      foo = 1
+    })
+
+    afterAll(function() {
+      foo = 0
+    })
+
+    it("sets the initial value of foo before specs run", function() {
+      expect(foo).to.eq(1)
+      foo += 1
+    })
+
+    it("does not reset foo between specs", function() {
+      expect(foo).to.eq(2)
+    })
+
+  })
+
+  // Nesting describe Blocks
+
+  describe("A spec", function() {
+    var foo
+
+    beforeEach(function() {
+      foo = 0
+      foo += 1
+    })
+
+    afterEach(function() {
+      foo = 0
+    })
+
+    it("is just a function, so it can contain any code", function() {
+      expect(foo).to.eq(1)
+    })
+
+    it("can have more than one expectation", function() {
+      expect(foo).to.eq(1)
+      expect(true).to.eq(true)
+    })
+
+    describe("nested inside a second describe", function() {
+      var bar
+
+      beforeEach(function() {
+        bar = 1
+      })
+
+      it("can reference both scopes as needed", function() {
+        expect(foo).to.eq(bar)
+      })
+
+    })
+
+  })
+
+})
