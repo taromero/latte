5f2588041b08126ac240176e784cd72950523119
Tomas Romero <canotto90@gmail.com>
2015-06-30 01:41:43 -0300

    Bump version 0.2.0

--- packages/latte/package.js
+++ packages/latte/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'canotto90:latte',
-  version: '0.1.1',
+  version: '0.2.0',
   summary: 'Run mocha-like specs in Meteor, without Velocity\'s overhead.',
   git: 'https://github.com/taromero/latte.git',
   documentation: '../../README.md'
