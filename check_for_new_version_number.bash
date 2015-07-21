c6220f1a44efb65e7bbabb9f01fc6e91f91580b6
Tomas Romero <canotto90@gmail.com>
2015-07-21 20:27:09 -0300

    Check for version number update on PRs

---
+++ check_for_new_version_number.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+diff <(git show master:packages/latte/package.js | grep -Po "version: \'.*?\',") <(git show HEAD:packages/latte/package.js | grep -Po "version: \'.*?\',")
+
+version_changed=$?; if [ $version_changed != 0 ]; then
+  exit 0
+else
+  echo 'Version needs to be bumped in order to merge PRs'
+  exit 1
+fi
