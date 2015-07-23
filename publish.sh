f05c2f31abb2274b8f8755d28c21fd8716237b95
Tomas Romero <canotto90@gmail.com>
2015-07-22 23:05:34 -0300

    Fix ci publish, again

--- publish.sh
+++ publish.sh
@@ -3,5 +3,5 @@
 # Only if not on a PR build => only master builds
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   echo 'Publishing package to Atmosphere'
-  printf $LI | meteor login && meteor publish
+  printf $LI | meteor login && cd packages/latte && meteor publish
 fi
