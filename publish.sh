69f547acbfe6426180af9a4873411f73c07b9d12
Tomas Romero <canotto90@gmail.com>
2015-07-22 22:10:17 -0300

    Add notification when publishing package from build

--- publish.sh
+++ publish.sh
@@ -2,5 +2,6 @@
 
 # Only if not on a PR build => only master builds
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+  echo 'Publishing package to Atmosphere'
   printf $LI | meteor login && meteor publish
 fi
